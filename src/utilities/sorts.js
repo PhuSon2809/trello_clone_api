d7e44bd6482346afe2a6be60fd1a8e93bec63378
Tran Phu Son <phuson2809@gmail.com>
2022-10-03 10:10:36 +0700

    Init project

---
+++ src/utilities/sorts.js
@@ -0,0 +1,20 @@
+/**
+ * Order an array of objects based on another array order
+ * ---
+ * if sort function
+ * 1. return a value < 0
+ *    -> a will come before b
+ * 2. return a value > 0
+ *    -> a will come after b
+ * 3. return 0
+ *    -> a and b will stay unchanged
+ * ---
+ * Function map là một function trả về 1 cái array
+ */
+
+const mapOrder = (array, order, key) => {
+  array.sort((a, b) => order.indexOf(a[key]) - order.indexOf(b[key]));
+  return array;
+};
+
+export { mapOrder };
